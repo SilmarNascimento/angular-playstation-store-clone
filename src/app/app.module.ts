53c4d38ecb11f2fff1193892cbb1bbe4105781d8
SilmarNascimento <silmarfnascimento@gmail.com>
2023-10-02 18:40:13 -0300

    feat: cria os componentes filhos de main-card e adiciona a pagina home ao projeto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,18 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MainCardComponent } from './components/main-card/main-card.component';
 import { MenuBarComponent } from './components/menu-bar/menu-bar.component';
+import { HomeComponent } from './pages/home/home.component';
+import { CardLabelComponent } from './components/main-card/card-label/card-label.component';
+import { CardPricingComponent } from './components/main-card/card-pricing/card-pricing.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MainCardComponent,
-    MenuBarComponent
+    MenuBarComponent,
+    HomeComponent,
+    CardLabelComponent,
+    CardPricingComponent
   ],
   imports: [
     BrowserModule,
