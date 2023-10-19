328f04c056b9e09f2897a72ed1e54bdc5a479f83
SilmarNascimento <silmarfnascimento@gmail.com>
2023-10-19 17:13:26 -0300

    refatora o código para passar a lógica dos componentes filhos para o pai

--- src/app/components/main-card/main-card.component.ts
+++ src/app/components/main-card/main-card.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-main-card',
   templateUrl: './main-card.component.html',
   styleUrls: ['./main-card.component.css']
 })
-export class MainCardComponent {
+export class MainCardComponent implements OnInit {
+  @Input()
+  gameCover: String = "";
+  @Input()
+  gameLabel: String = "";
+  @Input()
+  gameType: String = "";
+  @Input()
+  gamePrice: String = "";
 
+  constructor() {
+
+  }
+
+  ngOnInit(): void {
+
+  }
 }
