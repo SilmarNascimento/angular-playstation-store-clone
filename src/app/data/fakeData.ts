6f23d55fa51bf1ca9c30d180b35a5d4f8adc6b33
SilmarNascimento <silmarfnascimento@gmail.com>
2023-10-19 17:38:57 -0300

    feat: adiciona o schema da informação dos jogos, adiciona a coleção de informação sobre os jogos e implementa a importação dinâmica da informação dos jogos pelo componente Home

---
+++ src/app/data/fakeData.ts
@@ -0,0 +1,39 @@
+export const fakeData = [
+  {
+    "id": "1",
+    "gameCover": "assets/bt-1.jpg",
+    "gameLabel": "LEAVING SOON",
+    "gameType": "PS4",
+    "gamePrice": "R$ 199,00"
+  },{
+    "id": "2",
+    "gameCover": "assets/bt-4.jpg",
+    "gameLabel": "EXCLUSIVE",
+    "gameType": "DIGITAL",
+    "gamePrice": "R$ 139,00"
+  },{
+    "id": "3",
+    "gameCover": "assets/bt-5.jpg",
+    "gameLabel": "NEW",
+    "gameType": "PS4 PS5",
+    "gamePrice": "R$ 279,00"
+  },{
+    "id": "4",
+    "gameCover": "assets/ac-cover.jpg",
+    "gameLabel": "BEST OF THE YEAR",
+    "gameType": "DIGITAL | PS4 PS5",
+    "gamePrice": "R$ 159,00"
+  },{
+    "id": "5",
+    "gameCover": "assets/bt-bad-company-2.jpg",
+    "gameLabel": "NEW",
+    "gameType": "DIGITAL | PS4 PS5",
+    "gamePrice": "R$ 269,00"
+  },{
+    "id": "6",
+    "gameCover": "assets/bt-hardline.jpg",
+    "gameLabel": "EXCLUSIVE",
+    "gameType": "DIGITAL",
+    "gamePrice": "R$ 399,00"
+  },
+]
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import IGameSchema from 'src/app/components/Schema/gameSchema';
+import { fakeData } from 'src/app/data/fakeData';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  gameList: IGameSchema[] = [];
 
+  ngOnInit(): void {
+    this.gameList = fakeData;
+  }
 }
