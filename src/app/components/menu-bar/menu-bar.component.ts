857f603618d3fedce8c5b50f3c5f94137a186284
SilmarNascimento <silmarfnascimento@gmail.com>
2023-10-19 18:28:21 -0300

    feat: adiciona HomeComponent à pagina inicial

--- src/app/components/menu-bar/menu-bar.component.ts
+++ src/app/components/menu-bar/menu-bar.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { contactList } from 'src/app/data/socialContactList';
 
 @Component({
   selector: 'app-menu-bar',
   templateUrl: './menu-bar.component.html',
   styleUrls: ['./menu-bar.component.css']
 })
-export class MenuBarComponent {
+export class MenuBarComponent implements OnInit{
+  contacts: String[][] = [];
 
+  ngOnInit(): void {
+      this.contacts = contactList.map(contact => {
+        return contact.split(" - ");
+      });
+  }
 }
---
+++ src/app/data/socialContactList.ts
@@ -0,0 +1,5 @@
+export const contactList = [
+  'mail - mailto:silmarfnascimento@gmail.com',
+  'Github - https://github.com/SilmarNascimento',
+  'Linkedin - https://www.linkedin.com/in/silmarnascimento/',
+];
